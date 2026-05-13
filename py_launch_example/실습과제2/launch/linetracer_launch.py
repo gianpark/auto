1a6cb5c08e23f4914b0c09d66e16df456b17247c
gianpark <gianpark54@gmail.com>
2026-05-13 22:44:54 +0900

    Update linetracer_launch.py

--- py_launch_example/실습과제2/launch/linetracer_launch.py
+++ py_launch_example/실습과제2/launch/linetracer_launch.py
@@ -3,13 +3,11 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # 영상 기반 가짜 카메라 발행 노드
         Node(
             package='linetracer_sim',
             executable='video_publisher',
             name='video_publisher'
         ),
-        # 라인트레이서 메인 노드 (영상 구독 + 모터 제어)
         Node(
             package='linetracer_sim',
             executable='linetracer_sim',
